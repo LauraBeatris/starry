ef5299ba5df20f0ad3fd8eb2d13bed07cbfc5d63
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-09-25 20:29:02 -0300

    Introduce content box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-blue-900 flex justify-center items-center h-screen w-screen">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-export default function Home() {
+import { CSSProperties } from "react";
+
+function ContentBox() {
   return (
-    <p>Hello World</p>
-  )
+    <div
+      style={
+        {
+          "--background": "23 37 84",
+          "--highlight": "255 255 255",
+
+          "--bg-color":
+            "linear-gradient(rgb(var(--background)), rgb(var(--background)))",
+          "--border-color": `linear-gradient(145deg,
+            rgb(var(--highlight)) 0%,
+            rgb(var(--highlight) / 0.3) 33.33%,
+            rgb(var(--highlight) / 0.14) 66.67%,
+            rgb(var(--highlight) / 0.1) 100%)
+          `,
+        } as CSSProperties
+      }
+      className="flex aspect-[2/1] w-full max-w-md flex-col items-center justify-center rounded-xl border border-transparent p-8 text-center
+      [background:padding-box_var(--bg-color),border-box_var(--border-color)]"
+    >
+      <p className="text-xl font-medium text-white">Hello, gradient</p>
+    </div>
+  );
+}
+
+export default function Home() {
+  return <ContentBox />;
 }
