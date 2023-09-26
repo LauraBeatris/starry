2e689c066480d90797a0da31b204bcba810dbf2b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-09-25 21:24:04 -0300

    Apply animation to sparkles

--- src/components/Sparkles.tsx
+++ src/components/Sparkles.tsx
@@ -36,20 +36,22 @@ interface SparkleIconProps
 
 function SparkleIcon({ height, width, style, fill }: SparkleIconProps) {
   return (
-    <svg
-      width={width}
-      height={height}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 160 160"
-      className="absolute pointer-events-none z-10"
-      style={style}
-    >
-      <path
-        fill={fill}
-        d="M80 0s4.285 41.292 21.496 58.504C118.707 75.715 160 80 160 80s-41.293 4.285-58.504 21.496S80 160 80 160s-4.285-41.293-21.496-58.504C41.292 84.285 0 80 0 80s41.292-4.285 58.504-21.496C75.715 41.292 80 0 80 0z"
-      ></path>
-    </svg>
+    <div className="position pointer-events-none grow-and-shrink">
+      <svg
+        width={width}
+        height={height}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 160 160"
+        className="absolute pointer-events-none z-10 spin"
+        style={style}
+      >
+        <path
+          fill={fill}
+          d="M80 0s4.285 41.292 21.496 58.504C118.707 75.715 160 80 160 80s-41.293 4.285-58.504 21.496S80 160 80 160s-4.285-41.293-21.496-58.504C41.292 84.285 0 80 0 80s41.292-4.285 58.504-21.496C75.715 41.292 80 0 80 0z"
+        ></path>
+      </svg>
+    </div>
   );
 }
 
