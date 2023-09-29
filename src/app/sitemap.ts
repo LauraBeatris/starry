63562a920addd8070ba50da1a3e57d0abde016d5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-09-28 23:24:55 -0300

    Export dynamic metadata

---
+++ src/app/sitemap.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: "https://starry.vercel.app",
+      lastModified: new Date().toISOString(),
+    },
+    // TODO - Dynamically add URLs for pictures generated, querying from
+  ];
+}
