6283e630038608459fac445320210fc3b20ed16b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-23 19:14:24 -0300

    Structure card content

--- src/components/GitHubHoverCard/index.tsx
+++ src/components/GitHubHoverCard/index.tsx
@@ -2,11 +2,11 @@
 
 import * as HoverCard from '@radix-ui/react-hover-card';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './styles.module.css';
 import { GithubIcon } from '@/components/Icons/GitHubIcon';
 import { className } from '@/utils/className';
-
 
 const gitHubUrl = 'https://github.com/laurabeatris/starry';
 
@@ -31,13 +31,33 @@ export function GitHubHoverCard() {
             'rounded-lg p-5 w-72 bg-white shadow-lg',
           )}
         >
-          <div className="flex space-x-4 flex-col">
-            <Image
-              alt="GitHub Profile Pic"
-              width={20}
-              height={20}
-              src="https://avatars.githubusercontent.com/u/48022589?v=4"
-            />
+          <div className="flex flex-col space-y-2">
+            <div className="relative rounded-full w-12 h-12 overflow-hidden">
+              <Image
+                alt="GitHub Profile Pic"
+                src="https://avatars.githubusercontent.com/u/48022589?v=4"
+                layout="fill"
+              />
+            </div>
+
+            <div className="w-full flex flex-col space-y-2 align-start">
+              <div>
+                <div className="font-bold text-base text-gray-600">
+                  Laura Beatris
+                </div>
+                <div className="text-gray-500">LauraBeatris</div>
+              </div>
+
+              <div className="text-gray-500">
+                engineering{' '}
+                <Link
+                  className="text-gray-600 font-medium"
+                  href="https://workos.com"
+                >
+                  @workos
+                </Link>
+              </div>
+            </div>
           </div>
           <HoverCard.Arrow className="fill-current text-white" />
         </HoverCard.Content>
