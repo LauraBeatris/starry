7bd9910ad8faa27efb55f929b2a13748a29e17ca
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-25 19:55:54 -0300

    Remove `@radix-ui/react-icons`

--- src/app/(main)/_components/GitHubOAuthButton/index.tsx
+++ src/app/(main)/_components/GitHubOAuthButton/index.tsx
@@ -1,12 +1,11 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons';
-
 import { Button } from '@/components/Button';
+import { GithubIcon } from '@/components/Icons/GitHubIcon';
 
 export function GitHubOAuthButton() {
   return (
     <Button>
       <div className="w-full flex space-x-1 items-center">
-        <GitHubLogoIcon width={20} height={20} />{' '}
+        <GithubIcon width={20} height={20} />{' '}
         <p className="font-medium text-yellow-50">Continue with GitHub</p>
       </div>
     </Button>
