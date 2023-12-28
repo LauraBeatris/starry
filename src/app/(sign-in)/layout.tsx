58eedeedc3adebe5790ba5984faf55e58e335f1a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-28 18:29:15 -0300

    Move content to layout

--- src/app/(sign-in)/layout.tsx
+++ src/app/(sign-in)/layout.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 import { GitHubHoverCard } from '@/app/(sign-in)/_components/GitHubHoverCard';
+import TwitterIcon from '@/components/Icons/TwitterIcon';
 import { RadialGradientBox } from '@/components/RadialGradientBox';
+import { Sparkles } from '@/components/Sparkles';
 import { className } from '@/utils/className';
 import { interFont, playfairFont } from '@/utils/fonts';
 
@@ -31,7 +33,33 @@ export default function RootLayout({ children }: PropsWithChildren) {
 
         <main className="flex min-h-screen w-full flex-col items-center justify-center">
           <RadialGradientBox>
-            {children}
+            <section className="w-full h-full flex flex-col justify-center items-center flex-1 py-20">
+              <header>
+                <a
+                  // TODO - Reference tweet announcement
+                  href="https://x.com/lauradotjs"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="z-20	mx-auto flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden border rounded-2xl py-1 px-4 text-yellow-100 text-sm hover:scale-105 transition duration-300 ease-in-out"
+                >
+                  <TwitterIcon className="h-5 w-5" />
+
+                  <p className="text-sm font-semibold ">Introducing Starry</p>
+                </a>
+
+                <span className="text-8xl md:text-9xl text-center font-bold font-display tracking-tight text-yellow-50">
+                  <Sparkles>
+                    <Sparkles>Starry</Sparkles>
+                  </Sparkles>
+                </span>
+
+                <p className="text-center font-light text-yellow-50 [text-wrap:balance] md:text-xl pt-6 px-4">
+                  Generate pictures based on Van Gogh arts via AI model.
+                </p>
+              </header>
+
+              {children}
+            </section>
 
             <footer className="flex space-x-1 align-center pb-10">
               <p>🎨</p>
