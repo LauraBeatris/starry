d5101d01a8a89f7db64d0b1644e60f63e50bf37d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-12-27 22:22:37 -0300

    Generate GitHub OAuth authorization URL

--- src/app/(sign-in)/_components/GitHubOAuthButton/index.tsx
+++ src/app/(sign-in)/_components/GitHubOAuthButton/index.tsx
@@ -1,13 +1,20 @@
+import Link from 'next/link';
+
+import { getGitHubOAuthAuthorizationUrl } from '../../getAuthorizationUrl';
 import { Button } from '@/components/Button';
 import { GithubIcon } from '@/components/Icons/GitHubIcon';
 
 export function GitHubOAuthButton() {
+  const authorizationUrl = getGitHubOAuthAuthorizationUrl()
+
   return (
-    <Button>
-      <div className="w-full flex space-x-1 items-center">
-        <GithubIcon width={20} height={20} />{' '}
-        <p className="font-medium text-yellow-50">Continue with GitHub</p>
-      </div>
-    </Button>
+    <Link href={authorizationUrl}>
+      <Button>
+        <div className="w-full flex space-x-1 items-center">
+          <GithubIcon width={20} height={20} />{' '}
+          <p className="font-medium text-yellow-50">Continue with GitHub</p>
+        </div>
+      </Button>
+    </Link>
   );
 }
---
+++ src/app/(sign-in)/getAuthorizationUrl.ts
@@ -0,0 +1,12 @@
+import { WorkOS } from "@workos-inc/node";
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const clientId = process.env.WORKOS_CLIENT_ID;
+
+export function getGitHubOAuthAuthorizationUrl() {
+  return workos.userManagement.getAuthorizationUrl({
+    provider: 'GitHubOAuth',
+    redirectUri: 'http://localhost:3000',
+    clientId: clientId as string,
+  });
+}
