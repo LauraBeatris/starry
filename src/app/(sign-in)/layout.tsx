be712287b35d44f6abb2c315f9d43a408be8e28c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-01-05 11:15:02 -0300

    Add background with gradient

--- src/app/(sign-in)/layout.tsx
+++ src/app/(sign-in)/layout.tsx
@@ -1,7 +1,6 @@
 import '../globals.css';
 
 import { Metadata } from 'next';
-import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 import { GitHubHoverCard } from '@/app/(sign-in)/_components/GitHubHoverCard';
@@ -24,16 +23,16 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <html lang="en">
       <body
         className={className(
-          'bg-blue-900 flex justify-center items-center h-screen w-screen',
+          'bg-blue-900 flex justify-center items-center min-h-screen w-screen',
           playfairFont.variable,
           interFont.variable,
         )}
       >
         <GitHubHoverCard />
 
-        <main className="flex min-h-screen w-full flex-col items-center justify-center">
+        <main className="flex min-h-screen w-screen flex-col items-center justify-center">
           <RadialGradientBox>
-            <section className="w-full h-full flex flex-col justify-center items-center flex-1 py-20">
+            <section className="starry-background z-20 relative w-full h-full flex flex-col justify-center items-center flex-1 py-20">              
               <header>
                 <a
                   // TODO - Reference tweet announcement
@@ -60,50 +59,6 @@ export default function RootLayout({ children }: PropsWithChildren) {
 
               {children}
             </section>
-
-            <footer className="flex space-x-1 align-center pb-10">
-              <p>🎨</p>
-              <p className="font-display text-center [text-wrap:balance] md:text-md">
-                Created by{' '}
-                <Link
-                  className="hover:underline"
-                  href="https://laurabeatris.com"
-                  target="_blank"
-                >
-                  Laura Beatris
-                </Link>{' '}
-                - Powered by{' '}
-                <strong>
-                  <Link
-                    className="hover:underline"
-                    href="https://vercel.com"
-                    target="_blank"
-                  >
-                    Vercel
-                  </Link>
-                </strong>
-                ,{' '}
-                <strong>
-                  <Link
-                    className="hover:underline"
-                    href="https://workos.com"
-                    target="_blank"
-                  >
-                    WorkOS
-                  </Link>
-                </strong>{' '}
-                and{' '}
-                <strong>
-                  <Link
-                    className="hover:underline"
-                    href="https://replicate.com"
-                    target="_blank"
-                  >
-                    Replicate
-                  </Link>
-                </strong>
-              </p>
-            </footer>
           </RadialGradientBox>
         </main>
       </body>
