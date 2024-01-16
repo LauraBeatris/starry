bd9c6b126704e080407192d8b2d8ab11dde266d0
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-01-16 11:59:06 +0100

    Create folder for sign in group related images

--- src/app/(sign-in)/page.tsx
+++ src/app/(sign-in)/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { AuthButton } from './_components/AuthButton';
-import illustration from './starry-illustration.png';
+import illustration from './_images/starry-illustration.png';
 import { getAuthorizationUrl } from '@/app/auth';
 import { SparkleIcon } from '@/components/Icons/SparkleIcon';
 import { TwitterIcon } from '@/components/Icons/TwitterIcon';
