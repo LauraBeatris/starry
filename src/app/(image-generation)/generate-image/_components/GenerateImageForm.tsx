8bd7f07a2dfe0c3d9a2ea933957ceb5c121e5b0a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-01-27 21:31:02 -0300

    Remove popover

--- src/app/(image-generation)/generate-image/_components/GenerateImageForm.tsx
+++ src/app/(image-generation)/generate-image/_components/GenerateImageForm.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { PaintingPatternPicker, Popover } from './PaintingPatternPicker';
 import { PaintingUploader } from './PaintingUploader';
 
 export function GenerateImageForm() {
   return (
     <form className="z-30 flex flex-col items-center justify-center space-x-2 shadow-md">
-      {/* <PaintingPatternPicker /> */}
       <PaintingUploader />
     </form>
   );
