4cc1b1450a54cc0ff0dc0d6631dcbd546f1d46b5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-01-28 18:39:49 -0300

    Add `REPLICATE_WEBHOOK_URL` based on Vercel's environment

--- src/app/(image-generation)/actions.ts
+++ src/app/(image-generation)/actions.ts
@@ -1,5 +1,16 @@
+"use server";
+
+import { kv } from "@vercel/kv";
+import Replicate from 'replicate';
+
 import { getUser } from '../lib/auth';
+import { nanoid } from "../lib/nanoid";
 import { performRateLimitByUser } from '../lib/rateLimiter';
+import { REPLICATE_WEBHOOK_URL } from "../lib/replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
 
 export async function generateImage(form: FormData) {
   // TODO - Validate form data;
@@ -11,6 +22,26 @@ export async function generateImage(form: FormData) {
   }
 
   performRateLimitByUser(user);
+
+  const id = nanoid();
+  const image = "example";
+
+  await Promise.all([
+    kv.hset(id, {
+      // TODO - Pass real blob URL
+      image,
+    }),
+    replicate.predictions.create({
+      version:
+        "30c1d0b916a6f8efce20493f5d61ee27491ab2a60437c13c588468b9810ec23f",
+      input: {
+        prompt: "Convert this picture to the same style and colors as the Van Gogh's Starry night art",
+        image,
+      },
+      webhook: `${REPLICATE_WEBHOOK_URL}?id=${id}&secret=${process.env.REPLICATE_WEBHOOK_SECRET}`,
+      webhook_events_filter: ["completed"],
+    }),
+  ]);
 
   console.log('Generating image');
 }
---
+++ src/app/lib/replicate.ts
@@ -0,0 +1,6 @@
+export const REPLICATE_WEBHOOK_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? "https://starry.vercel.app/api/webhook"
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/webhook`
+    : `${process.env.NGROK_URL}/api/webhook`;
