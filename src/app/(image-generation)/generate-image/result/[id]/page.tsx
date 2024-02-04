f2d487ecf8fa538aff8117daf3fd52f5a4986f18
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-02-04 18:34:46 -0300

    Add link to go back

--- src/app/(image-generation)/generate-image/result/[id]/page.tsx
+++ src/app/(image-generation)/generate-image/result/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { kv } from '@vercel/kv';
 import { Metadata } from 'next';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import { CopyButton } from '../../_components/CopyButton';
 import { DownloadButton } from '../../_components/DownloadButton';
 import { ImageResult } from '../../_components/ImageResult';
+import { ArrowLeftIcon } from '@/app/ui/Icons/ArrowLeftIcon';
 
 export async function generateMetadata({
   params,
@@ -59,6 +61,15 @@ export default async function GenerateImageResultPage({
   return (
     <div className="relative w-full p-4">
       <div className="relative w-full">
+        <div className="absolute left-5 top-5 z-50 flex">
+          <Link
+            href="/generate-image"
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-gray-200 bg-white shadow-sm transition-all hover:scale-105 active:scale-95"
+          >
+            <ArrowLeftIcon className="h-4 w-4 text-gray-600" />
+          </Link>
+        </div>
+
         <div className="absolute right-5 top-5 z-50 flex space-x-2">
           <CopyButton
             imageId={params.id}
