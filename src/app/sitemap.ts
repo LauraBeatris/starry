458df14cd7bdc682b8133d30a60c78f61566fa51
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-02-06 09:51:08 -0300

    Generate sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,31 @@
-import { MetadataRoute } from 'next';
+import { kv } from "@vercel/kv";
+import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const ids: string[] = [];
+  let cursor = 0;
+  const maxKeys = 50000;
+  
+  while (ids.length < maxKeys) {
+    const [nextCursor, keys] = await kv.scan(cursor, {
+      match: "*",
+      count: 1000,
+    });
+    if (!keys.length || nextCursor === 0) break; 
+    
+    ids.push(...keys.slice(0, maxKeys - ids.length)); 
+    cursor = nextCursor;
+  }
+
+
   return [
     {
-      url: 'https://starry.vercel.app',
+      url: "https://my-starry.com",
       lastModified: new Date().toISOString(),
     },
-    // TODO - Dynamically add URLs for pictures generated, querying from generated arts
+    ...ids.map((id) => ({
+      url: `https://my-starry.com/generate-image/result/${id}`,
+      lastModified: new Date().toISOString(),
+    })),
   ];
 }
