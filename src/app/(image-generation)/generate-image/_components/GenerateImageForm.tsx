e0bbd684fff9f45baf1ec9660f90791c5e4c2b62
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-02-06 11:52:53 -0300

    Move prompt generation to server component

--- src/app/(image-generation)/generate-image/_components/GenerateImageForm.tsx
+++ src/app/(image-generation)/generate-image/_components/GenerateImageForm.tsx
@@ -1,21 +1,22 @@
+// @ts-ignore
+import promptmaker from 'promptmaker';
+
 import { PromptForm } from './PromptForm';
 
 // TODO - Improve props definition to avoid impossible states
 type GenerateImageFormProps = {
-  initialPromptText?: string;
   imagePlaceholder?: string;
   remainingGenerations: number;
 };
 
 export function GenerateImageForm({
-  initialPromptText,
   remainingGenerations,
 }: GenerateImageFormProps) {
   return (
     <section className="md:p-reset z-10 w-full px-2">
       <PromptForm
         disabled={remainingGenerations <= 0}
-        initialPromptText={initialPromptText}
+        initialPromptText={promptmaker()}
       />
     </section>
   );
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,26 +1,25 @@
-import { kv } from "@vercel/kv";
-import { MetadataRoute } from "next";
+import { kv } from '@vercel/kv';
+import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const ids: string[] = [];
   let cursor = 0;
   const maxKeys = 50000;
-  
+
   while (ids.length < maxKeys) {
     const [nextCursor, keys] = await kv.scan(cursor, {
-      match: "*",
+      match: '*',
       count: 1000,
     });
-    if (!keys.length || nextCursor === 0) break; 
-    
-    ids.push(...keys.slice(0, maxKeys - ids.length)); 
+    if (!keys.length || nextCursor === 0) break;
+
+    ids.push(...keys.slice(0, maxKeys - ids.length));
     cursor = nextCursor;
   }
 
-
   return [
     {
-      url: "https://my-starry.com",
+      url: 'https://my-starry.com',
       lastModified: new Date().toISOString(),
     },
     ...ids.map((id) => ({
